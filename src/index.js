6879d37b6a8e2e4efc3f90a89e56fb58e42e68ca
assinaturasnetflix <jonaspedroqt@gmail.com>
2025-05-11 22:05:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Tailwind CSS import
+import App from './App';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  React.createElement(React.StrictMode, null,
+    React.createElement(App, null)
+  )
+);
